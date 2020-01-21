f626995e13ebea44f40ad72909645f4df97e1103
unknown <travisle22@gmail.com>
2020-01-21 15:19:51 -0800

    Testing run.py

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
+
+# config
+DEBUG = True
 
 app = Flask(__name__)
+app.config.from_object(__name__)
 
+
+@app.route('/test', methods=['GET'])
+def test():
+  return jsonify('help!')
 if __name__ == '__main__':
   app.run('0.0.0.0')
