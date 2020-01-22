c3a450f6cc90c808ef76c0d8a2f46e62e120be09
Lyman Shen <lymanshen@localhost.localdomain>
2020-01-21 16:55:27 -0800

    Allow Flask to use VueJs output as template

--- backend/run.py
+++ backend/run.py
@@ -1,14 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
+from flask_cors import CORS
 
-# config
+#config
 DEBUG = True
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='../dist',static_folder='../static')
 app.config.from_object(__name__)
 
+CORS(app,resources={r'/*': {'origins':'*'}})
 
 @app.route('/test', methods=['GET'])
 def test():
   return jsonify('help!')
+
+@app.route('/',methods=['GET'])
+def home():
+    return render_template('index.html')
+
 if __name__ == '__main__':
   app.run('0.0.0.0')
