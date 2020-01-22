bb57663c8f6e081b5b12435c8eddf8007c812254
unknown <travisle22@gmail.com>
2020-01-21 16:57:47 -0800

    changed backend a bit

--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, current_app, jsonify, render_template
+import os
 
 # config
 DEBUG = True
@@ -6,9 +7,15 @@ DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+jinja_options = app.jinja_options.copy()
+jinja_options.update(dict(
+    variable_start_string='%%',
+    variable_end_string='%%'
+))
+app.jinja_options = jinja_options
 
 @app.route('/test', methods=['GET'])
 def test():
-  return jsonify('help!')
+  return render_template(entry)
 if __name__ == '__main__':
   app.run('0.0.0.0')
