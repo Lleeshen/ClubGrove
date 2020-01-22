0a43b7c152f6df853fd6759f3c002ce636420118
unknown <travisle22@gmail.com>
2020-01-21 18:03:47 -0800

    Insert in some python files Added the ability to start from root

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,4 +3,17 @@ const path = require('path');
 module.exports = {
   outputDir: path.resolve(__dirname,'../dist'),
   assetsDir: '../static',
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Index Page',
+    }
+  }
 };
---
+++ start.py
@@ -0,0 +1,5 @@
+import os
+import backend.run as ok
+
+ok.app.run(port =3000)
+
