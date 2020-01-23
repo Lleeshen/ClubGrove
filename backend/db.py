b15b347ad379cdaeee8b1aa0c58c603b5fecb149
unknown <travisle22@gmail.com>
2020-01-23 10:12:34 -0800

    Added a ini file thing changed git .gitignore

--- backend/db.py
+++ backend/db.py
@@ -1,7 +1,13 @@
 import psycopg2
+import configparser
+import os
+
+parser = configparser.ConfigParser()
+parser.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'dbfile.ini'))
+
 
 con = psycopg2.connect(dbname='testing', user='postgres',
-        host='localhost', password= '')
+        host='localhost', password= parser.get('postgres', 'password'))
 cur = con.cursor()
 cur.execute("insert into ok values (1, 't', 'stuff')")
 cur.execute("select * from ok")
