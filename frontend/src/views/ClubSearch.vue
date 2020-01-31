1023483c6da02f84eba37ae0de4d70756e5110d6
Lyman Shen <lymanshen@localhost.localdomain>
2020-01-31 15:22:53 -0800

    This adds paths

---
+++ frontend/src/views/ClubSearch.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="searchBar">
+    <span>clubName</span>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Search Clubs'
+}
+</script>
+
+<style scoped>
+#searchBar {
+  height: 40px;
+  background-color: green;
+}
+</style>
