800de92487088dfb2f83cdc733bd6ef0eb7f2a25
Lyman Shen <lymanshen@localhost.localdomain>
2020-02-03 14:36:43 -0800

    A beta version of search bar

--- frontend/src/views/ClubSearch.vue
+++ frontend/src/views/ClubSearch.vue
@@ -1,10 +1,31 @@
 <template>
   <div>
     <div id="searchBar">
-      <span>Club Name</span>
-      <span>Club Categories</span>
-      <span>Sort Option </span>
-      <span>Search </span>
+      <form class="form-inline" action="/club">
+        <div class="form-group">
+          <label for="searchTerm">Club name:</label>
+          <input type="text" class="form-control" id="searchTerm">
+        </div>
+        <div class="form-group">
+          <label for="clubCat">Select Categories</label>
+          <select multiple class="form-control" id="clubCat">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <!-- Club Categories from database goes here -->
+          </select>
+        </div>
+        <div class="form-group">
+          <label for="clubSort">Select Categories:</label>
+          <select class="form-control" id="clubSort">
+            <option>Alphabetical ascending</option>
+            <option>Alphabetical descending</option>
+            <option>Similarity to search term??</option>
+          </select>
+        </div>
+        <button type="submit" class="btn">Submit</button>
+      </form>
     </div>
     <h2> Club Results </h2>
   </div>
@@ -20,7 +41,12 @@ export default {
 
 <style scoped>
 #searchBar {
-  height: 40px;
+  height: auto;
   background-color: green;
 }
+
+form {
+  margin: 0 auto;
+}
+
 </style>
