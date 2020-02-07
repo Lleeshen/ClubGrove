69bf654cb0cc858b9b2006408ba38b433ee6e95b
unknown <travisle22@gmail.com>
2020-02-07 15:12:08 -0800

    Added Empty Club Page

---
+++ frontend/src/views/ClubPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from 'axios'
+
+export default {
+  name: 'ClubPage',
+  data() {
+    return {
+      //Replace this with actual club results
+      items: null
+    }
+  },
+  mounted() {
+    axios
+      .get('http://localhost:5000/db/view/club')
+      .then(response => {this.items = response.data;
+       console.log(response) })
+      .catch(error => {console.log(error)})
+  }
+}
+
+</script>
+
+
+</style>
