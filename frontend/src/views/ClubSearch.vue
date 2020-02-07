cbac77335fb3eb8392958b7c8ce2da4919550b9c
Lyman Shen <lymanshen@localhost.localdomain>
2020-02-06 16:44:18 -0800

    Spacing in Search stuff, change button to say search

--- frontend/src/views/ClubSearch.vue
+++ frontend/src/views/ClubSearch.vue
@@ -3,12 +3,12 @@
     <div id="searchBar">
       <b-form inline action="/club">
         <label class="sr-only" for="searchTerm">Club Name</label>
-        <b-form-input v-model="text" id="searchTerm" placeholder="Club name"></b-form-input>
+        <b-form-input class="searchFormElt" v-model="text" id="searchTerm" placeholder="Club name"></b-form-input>
         <label class="sr-only" for="clubCategory">Club Category</label>
-        <b-form-select v-model="selectedClubCat" :options="clubCatOptions" id="clubCategory"></b-form-select>
+        <b-form-select class="searchFormElt" v-model="selectedClubCat" :options="clubCatOptions" id="clubCategory"></b-form-select>
         <label for="sortOption">Sorting Option</label>
-        <b-form-select v-model="selectedSortOption" :options="sortOptions" id="sortOption"></b-form-select>
-        <b-button variant="secondary">Submit</b-button>
+        <b-form-select class="searchFormElt" v-model="selectedSortOption" :options="sortOptions" id="sortOption"></b-form-select>
+        <b-button class="searchFormElt" variant="secondary">Search</b-button>
       </b-form>
     </div>
     <h2> Club Results </h2>
@@ -19,7 +19,6 @@
 <script>
 // @ is an alias to /src
 
-import axios from 'axios'
 export default {
   name: 'searchClubs',
   data() {
@@ -45,13 +44,6 @@ export default {
         { clubName: 'Jami', clubDescription: 'Carney' }
       ]
     }
-  },
-  mounted() {
-    axios
-      .get('http://localhost:5000/db/view/club')
-      .then(response => {this.items = response.data;
-       console.log(response) })
-      .catch(error => {console.log(error)})
   }
 }
 
@@ -72,6 +64,10 @@ h2 {
   text-align: left;
   margin-left: 5%;
   margin-top: 20px;
+}
+
+.searchFormElt {
+  margin: 0 10px 0 10px;
 }
 
 </style>
